14c0acb6902d8b324672ca197aa692e578f67707
Jang Rush <weakish@gmail.com>
2022-09-07 12:17:18 +0000

    :new: support [inline](links)

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,24 @@
 const linkifyStr = require('linkify-string')
 
+/** @type {function(number, number): number} */
+function linkIt(text) {
+    inlineLink = /\[([^\]]+)\]\(([^<)]+)\)/gm
+    return linkifyStr(text).replaceAll(inlineLink, '<a href="$2">$1</a>')
+}
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addTemplateFormats("txt");
     eleventyConfig.addExtension("txt", {
         compile: async (inputContent, inputPath) => {
-            const html=`<!doctype html><html lang=en>
+            return async () => `<!doctype html><html lang=en>
             <head>
                 <link rel=icon href=data:,>
                 <title>${inputPath.split('/').at(-1).split('.').at(0)}</title>
             </head>
             <body>
-                <pre style=font:unset>
-                ${linkifyStr(inputContent)}
-                </pre>
+            <pre>${linkIt(inputContent)}</pre>
             </body>
             </html>`
-            return async () => html
         }
     })
 }
