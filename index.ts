14862ae1f543cfedd15eea974d2cce017f06ef0a
Jang Rush <weakish@gmail.com>
2022-09-08 12:41:37 +0000

    :new: extractTitle

--- index.ts
+++ index.ts
@@ -1,4 +1,43 @@
-import linkifyStr from 'linkify-string'
+import linkifyStr from "linkify-string";
+
+function trim_start(line: string): [string, boolean] {
+  const trimmed: string = line.trimStart();
+  if (trimmed.startsWith("# ")) {
+    return [trimmed.slice(2), true];
+  } else {
+    return [trimmed, false];
+  }
+}
+
+export function extractTitle(text: string): [string, string] {
+  const first_newline_character_position: number = text.indexOf("\n");
+
+  if (first_newline_character_position === -1) {
+    const text_is_a_very_long_line_without_newline_character: boolean =
+      text.length > 50;
+    if (text_is_a_very_long_line_without_newline_character) {
+      const first_line: string = text.slice(0, 50);
+      const remaing_text: string = text.slice(50);
+      return [trim_start(first_line)[0], remaing_text];
+    } else {
+      return [trim_start(text)[0], ""];
+    }
+  } else if (first_newline_character_position > 50) {
+    const truncated: string = text.slice(0, 50);
+    const trimmed: [string, boolean] = trim_start(truncated);
+    const trimmed_line: string = trimmed[0];
+    const is_trimmed: boolean = trimmed[1];
+    if (is_trimmed) {
+      return [trimmed_line, trimmed_line + text.slice(50)];
+    } else {
+      return [truncated, text];
+    }
+  } else {
+    const first_line: string = text.slice(0, first_newline_character_position);
+    const remaing_text: string = text.slice(first_newline_character_position);
+    return [trim_start(first_line)[0], remaing_text];
+  }
+}
 
 export function linkIt(text: string): string {
     const inline_link: RegExp = /\[([^\]]+)\]\(([^<)]+)\)/gm
