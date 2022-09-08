f29c262d29781820388b6586014b458f55861f9f
Jang Rush <weakish@gmail.com>
2022-09-08 12:42:26 +0000

    :map: first tests

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,18 @@
 import tap from "tap";
+import { extractTitle } from "./index.js";
 
-tap.pass("pass");
+const sample_license: string = `Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.`;
+const expected_license_title: string =
+  "Permission to use, copy, modify, and/or distribute";
+
+tap.equal(extractTitle(sample_license)[0], expected_license_title);
+tap.equal(extractTitle(sample_license)[1], sample_license);
