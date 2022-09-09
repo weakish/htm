7ac32b36b5d14ada1ba997d0485047550417f8d2
Jang Rush <weakish@gmail.com>
2022-09-09 14:52:40 +0000

    :zzz: syntax typo in scripts/doc.sh

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -2,7 +2,7 @@
 if [ -n "$RENDER" ]; then
   curl https://bun.sh/install | bash
   bun ./scripts/doc.ts
-elseif [ "$(uname)" = "Linux" -a "$(uname -m)" = "x86_64" ]; then
+elif [ "$(uname)" = "Linux" -a "$(uname -m)" = "x86_64" ]; then
   ./scripts/bun-linux-amd64 ./scripts/doc.ts
 else
   echo 'Currently, only Linux amd64 is supported.'
