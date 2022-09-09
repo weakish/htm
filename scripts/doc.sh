9363ce6d3d9c575379c45225471d21f26c3618b0
Jang Rush <weakish@gmail.com>
2022-09-09 15:27:16 +0000

    :zzz: doc bun GLIBC issue

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-if [ -n "$RENDER" ]; then
-  curl https://bun.sh/install | bash
-  $HOME/.bun/bin/bun ./scripts/doc.ts
-elif [ "$(uname)" = "Linux" -a "$(uname -m)" = "x86_64" ]; then
+if [ "$(uname)" = "Linux" -a "$(uname -m)" = "x86_64" ]; then
   ./scripts/bun-linux-amd64 ./scripts/doc.ts
 else
   echo 'Currently, only Linux amd64 is supported.'
   echo 'Pull request to support other platform is welcome.'
+  echo 'Some Linux distributions cannot run bun.sh due to GLIBC version mismatch.'
+  echo 'See https://github.com/oven-sh/bun/issues/255'
 fi
