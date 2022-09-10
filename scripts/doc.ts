c35c22ecd0af6443009ecec47c1f3da63de7bf14
Jang Rush <weakish@gmail.com>
2022-09-10 16:44:40 +0000

    :zzz: convert README in different html variants

--- scripts/doc.ts
+++ scripts/doc.ts
@@ -1,6 +1,10 @@
 import { readFileSync, writeFileSync } from "fs";
-import { htm } from "../src/index.bs.js";
+import { htm, html } from "../src/index.bs.js";
 
 const text: string = readFileSync("README.txt", "utf8");
-const html: string = htm(text);
-writeFileSync("_site/index.html", html);
+const html2: string = htm(text);
+writeFileSync("_site/index.html", html2);
+
+writeFileSync("_site/html-tags.html", html(text, "tags"));
+writeFileSync("_site/iso.html", html(text, "iso"));
+writeFileSync("_site/html5.html", html(text, "html5"));
